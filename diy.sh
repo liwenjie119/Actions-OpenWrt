047cf8f3e186f7959e9b85d54971b479505e0fc1
liwenjie119 <liwenjie119@126.com>
2022-07-23 21:53:10 +0800

    fix and update

--- diy.sh
+++ diy.sh
@@ -34,7 +34,7 @@ git clone https://github.com/yaof2/luci-app-ikoolproxy package/myapp/luci-app-ik
 git clone https://github.com/destan19/OpenAppFilter.git package/myapp/OpenAppFilter
 #svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/myapp/aliyundrive-webdav
 #luci-theme-argon
-rm -rf package/lean/luci-theme-argon  
+rm -rf feeds/luci/themes/luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/myapp/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/myapp/luci-app-argon-config
 fi
