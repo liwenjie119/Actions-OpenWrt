0a0964e2881cb9acb9fe2a6a32b5d7a9de4bfb41
liwenjie119 <liwenjie119@126.com>
2024-01-13 09:02:12 +0800

    try 2

--- diy-360-t7-108M.sh
+++ diy-360-t7-108M.sh
@@ -13,8 +13,26 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+function merge_package(){
+    repo=echo $1 | rev | cut -d'/' -f 1 | rev
+    pkg=echo $2 | rev | cut -d'/' -f 1 | rev
+    git clone --depth=1 --single-branch $1
+    mv $2 package/myapp/
+    rm -rf $repo
+}
+rm -rf package/myapp; mkdir package/myapp
+
+# v2ray-server 
+merge_package https://github.com/coolsnowwolf/luci luci/applications/luci-app-v2ray-server
+
+
 # Add a feed source
 git clone https://github.com/kongfl888/luci-app-adguardhome package/myapp/luci-app-adguardhome
 git clone https://github.com/yaof2/luci-app-ikoolproxy package/myapp/luci-app-ikoolproxy
+
+
+
+
+
 
 
